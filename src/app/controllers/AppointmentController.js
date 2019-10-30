b943395278a8d04fb243e9747e97f2bad36f5f75
Gustavo Prizon <gustavoprizon@gmail.com>
2019-10-30 17:15:07 -0300

    implementada validação de data e hora

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import User from '../models/User';
 import Appointment from '../models/Appointment';
 
@@ -29,6 +30,28 @@ class AppintmentConttoller {
       return res
         .status(401)
         .json({ error: 'You can only create appointments with providers' });
+    }
+
+    // verifica se a data do appointment está antes da hora atual
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past datesare not permitted' });
+    }
+
+    // verifica se a data está disponivel
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return res
+        .status(400)
+        .json({ error: 'Appointment date is not available.' });
     }
 
     const appointment = await Appointment.create({
