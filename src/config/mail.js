dddf413641bd442431848596e78c9e9987270512
Gustavo Prizon <gustavoprizon@gmail.com>
2019-11-05 23:09:53 -0300

    add send email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '71044ccacf5d16',
+    pass: '8bbb4ffe45dd45',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
